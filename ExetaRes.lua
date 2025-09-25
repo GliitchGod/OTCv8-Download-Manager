50f14e9188f9af0e7eed7a6363f886519c496cba
GliitchGod <alenalky123@gmail.com>
2025-09-25 22:49:52 +0200

    Create ExetaRes.lua

---
+++ ExetaRes.lua
@@ -0,0 +1,13 @@
+local macroName = 'Exeta Res';
+local macroDelay = 100;
+local tabName = setDefaultTab('A');
+
+
+macro(macroDelay, macroName, function()
+    if g_game.isAttacking() and 
+    distanceFromPlayer(g_game.getAttackingCreature():getPosition()) <= 1 and 
+    manapercent() > 30 then
+        say("Exeta Res")
+        delay(5000)
+    end
+end, tabName)
