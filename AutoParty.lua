3af6a12af78e22710651511e8e9f2aab99b82dd6
GliitchGod <alenalky123@gmail.com>
2025-09-27 00:12:34 +0200

    Update AutoParty.lua

--- AutoParty.lua
+++ AutoParty.lua
@@ -3,6 +3,7 @@
   If you want to support my work, feel free to donate at https://trainorcreations.com/donate
   PS. Stop ripping off my work and selling it as your own.
 ]]--
+local tabName = setDefaultTab('PT');
 local panelName = "autoParty"
 local autopartyui = setupUI([[
 Panel
