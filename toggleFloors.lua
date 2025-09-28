cdb8381598edd11fa34c950d71c9cafb557ef1d3
GliitchGod <alenalky123@gmail.com>
2025-09-28 13:31:09 +0200

    Create toggleFloors.lua

---
+++ toggleFloors.lua
@@ -0,0 +1,19 @@
+onPlayerPositionChange(function(pos)
+    if storage.limitFloor then
+        local gameMapPanel = modules.game_interface.getMapPanel()
+        if gameMapPanel then gameMapPanel:lockVisibleFloor(pos.z) end
+    end
+end)
+local switch = addSwitch("limitFloor", "Don't show higher floors",
+                         function(widget)
+    widget:setOn(not widget:isOn())
+    storage.limitFloor = widget:isOn()
+    local gameMapPanel = modules.game_interface.getMapPanel()
+    if gameMapPanel then
+        if storage.limitFloor then
+            gameMapPanel:lockVisibleFloor(posz())
+        else
+            gameMapPanel:unlockVisibleFloor()
+        end
+    end
+end)
